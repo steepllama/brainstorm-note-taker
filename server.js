c6a12f382f4ae5c35c496b1f0f73df98f47291b4
steepllama <steepllama@gmail.com>
2022-01-28 20:11:17 -0600

    added the libraries and the port for the html pages

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+
+
+
+app.listen(PORT, function() {
+    console.log(`app is listening on port: ${PORT}`)
+})
